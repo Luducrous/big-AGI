32dc38024eca289b6d28b91289698c2d2c6eeabb
Stijn Schuurman <s.j.schuurman@student.utwente.nl>
2024-12-16 08:53:23 +0100

    update server

--- src/modules/llms/vendors/python/SimplePythonSourceSetup.tsx
+++ src/modules/llms/vendors/python/SimplePythonSourceSetup.tsx
@@ -30,7 +30,7 @@ export function SimplePythonSourceSetup(props: { sourceId: DModelSourceId }) {
         <FormTextField
             autoCompleteId='buddygpt-server'
             title='Server URL'
-            placeholder='https://5ea8-2a02-a444-4e05-0-c858-825a-131f-acca.ngrok-free.app'
+            placeholder='https://cd76-2a02-a464-d7fc-1-1d4-2bdb-f91-2355.ngrok-free.app'
             value={oaiHost}
             onChange={text => updateSetup({ oaiHost: text })}
         />
--- src/modules/llms/vendors/python/simplepython.vendor.ts
+++ src/modules/llms/vendors/python/simplepython.vendor.ts
@@ -23,14 +23,14 @@ export const ModelVendorSimplePython: IModelVendor<SourceSetupSimplePython, Open
 
     // Setup
     initializeSetup: () => ({
-        oaiHost: 'https://5ea8-2a02-a444-4e05-0-c858-825a-131f-acca.ngrok-free.app',
+        oaiHost: 'https://cd76-2a02-a464-d7fc-1-1d4-2bdb-f91-2355.ngrok-free.app',
     }),
 
     getTransportAccess: (partialSetup) => ({
         dialect: 'zwiersai' as const,
         oaiKey: '',
         oaiOrg: '',
-        oaiHost: partialSetup?.oaiHost || 'https://5ea8-2a02-a444-4e05-0-c858-825a-131f-acca.ngrok-free.app',
+        oaiHost: partialSetup?.oaiHost || 'https://cd76-2a02-a464-d7fc-1-1d4-2bdb-f91-2355.ngrok-free.app',
         heliKey: '',
         moderationCheck: false,
     }),
